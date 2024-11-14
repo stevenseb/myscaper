ec8319a2dcc8264889662b892db903698d7ce48a
stevenseb <stevenseb@hotmail.com>
2024-11-14 00:00:04 -0800

    updated index.ts

--- db/seeds/index.ts
+++ db/seeds/index.ts
@@ -1,15 +1,23 @@
 // db/seeds/index.ts
 
 import * as dotenv from "dotenv";
-dotenv.config({ path: "../.env.local" });
+dotenv.config({ path: "../../.env.local" });
 
-import { seedUsers } from "./seedUser";
-import { seedProviders } from "./seedProvider";
+import { seedUsers } from "./seedUser.js";
+import { seedProviders } from "./seedProvider.js";
+import { seedJobs } from "./seedJob.js";
+import { seedPosts } from "./seedPost.js";
+import { seedReviews } from "./seedReview.js";
+import { seedMessages } from "./seedMessage.js";
 
 const runAllSeeds = async () => {
   try {
     await seedUsers();
     await seedProviders();
+    await seedJobs();
+    await seedPosts();
+    await seedReviews();
+    await seedMessages();
 
     console.log("All seeds completed successfully.");
   } catch (error) {
